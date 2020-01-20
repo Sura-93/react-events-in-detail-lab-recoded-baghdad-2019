ceb211afa607117700299248fe648f8217e69a9a
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-20 12:49:35 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -2,10 +2,10 @@
 import React, { Component } from "react";
 
 class DelayedButton extends Component {
-  handleClick = event => {
-    event.presist();
+  handleClick = (event) => {
+    event.presist()
     setTimeout(() => this.props.onDelayedClick(event), this.props.delay);
-  };
+  }
   render() {
     return <button onClick={this.handleClick}></button>;
   }
